bbd186ac5d2effb029a31546cf81a97dc1189ead
Elliott Bussell <ehb31@cam.ac.uk>
2019-04-10 11:55:18 +0100

    Updated param uncertainty

--- param_uncertainty.py
+++ param_uncertainty.py
@@ -13,6 +13,9 @@ import parameters
 import visualisation
 import mpc
 
+def even_policy(time):
+    return np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
+
 def generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev):
 
     model = ms_sim.MixedStandSimulator(setup, params)
@@ -20,7 +23,11 @@ def generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev):
     ncells = np.product(setup['landscape_dims'])
 
     # Create parameter error distribution
-    error_dist = truncnorm(-1.0/standard_dev, np.inf, loc=1.0, scale=standard_dev)
+    if standard_dev != 0.0:
+        error_dist = truncnorm(-1.0/standard_dev, np.inf, loc=1.0, scale=standard_dev)
+        error_samples = np.reshape(error_dist.rvs(size=n_ensemble_runs*7), (n_ensemble_runs, 7))
+    else:
+        error_samples = np.ones((n_ensemble_runs, 7))
 
     # Sample from parameter distribution and run simulations
     baseline_beta = np.zeros(7)
@@ -28,7 +35,6 @@ def generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev):
     baseline_beta[4] = params['inf_bay_to_tanoak']
     baseline_beta[5] = params['inf_tanoak_to_bay']
     baseline_beta[6] = params['inf_bay_to_bay']
-    error_samples = np.reshape(error_dist.rvs(size=n_ensemble_runs*7), (n_ensemble_runs, 7))
     parameter_samples = error_samples * baseline_beta
 
     simulation_runs = np.zeros(
@@ -41,13 +47,13 @@ def generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev):
         model.params['inf_tanoak_to_bay'] = parameter_samples[i, 5]
         model.params['inf_bay_to_bay'] = parameter_samples[i, 6]
 
-        sim_run = model.run_policy()
+        sim_run, *_ = model.run_policy()
         simulation_runs[i] = np.sum(sim_run.reshape((ncells, 15, -1)), axis=0) / ncells
 
         model.params['inf_bay_to_tanoak'] = 0.0
         model.params['inf_tanoak_to_bay'] = 0.0
 
-        sim_run = model.run_policy()
+        sim_run, *_ = model.run_policy()
         simulation_runs_no_cross_trans[i] = np.sum(
             sim_run.reshape((ncells, 15, -1)), axis=0) / ncells
 
@@ -70,7 +76,7 @@ def generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev):
     bounds = [(1e-10, 20), (1e-10, 20), (1e-10, 20), (1e-10, 20), (0.0, 0.0),
               (0.0, 0.0), (1e-10, 20)]
 
-    approx_model, beta = fitter.fit(start, bounds, show_plot=True, scale=True, averaged=True,
+    approx_model, beta = fitter.fit(start, bounds, show_plot=False, scale=True, averaged=True,
                                     dataset=simulation_runs_no_cross_trans)
     print("Done fit within tanoak...")
     print("Beta: {0}".format(beta))
@@ -82,7 +88,7 @@ def generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev):
         start[i+1] /= start[0]
         bounds[i+1] = (start[i+1], start[i+1])
     bounds[4] = (1e-10, 20)
-    approx_model, beta = fitter.fit(start, bounds, show_plot=True, scale=True, averaged=True,
+    approx_model, beta = fitter.fit(start, bounds, show_plot=False, scale=True, averaged=True,
                                     dataset=simulation_runs)
     print("Done final fit.")
     print("Beta: {0}".format(beta))
@@ -97,13 +103,14 @@ def run_optimisations(ensemble_and_fit, params, setup, n_optim_runs, standard_de
     approx_model = ms_approx.MixedStandApprox(setup, params, ensemble_and_fit['fit'])
     sim_model = ms_sim.MixedStandSimulator(setup, params)
 
-    def even_policy(time):
-        return np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
-
     _, ol_control, _ = approx_model.optimise(n_stages=20, init_policy=even_policy)
 
     # Create parameter error distribution
-    error_dist = truncnorm(-1.0/standard_dev, np.inf, loc=1.0, scale=standard_dev)
+    if standard_dev != 0.0:
+        error_dist = truncnorm(-1.0/standard_dev, np.inf, loc=1.0, scale=standard_dev)
+        error_samples = np.reshape(error_dist.rvs(size=n_optim_runs*7), (n_optim_runs, 7))
+    else:
+        error_samples = np.ones((n_optim_runs, 7))
 
     # Sample from parameter distribution and run simulations
     baseline_beta = np.zeros(7)
@@ -111,7 +118,6 @@ def run_optimisations(ensemble_and_fit, params, setup, n_optim_runs, standard_de
     baseline_beta[4] = params['inf_bay_to_tanoak']
     baseline_beta[5] = params['inf_tanoak_to_bay']
     baseline_beta[6] = params['inf_bay_to_bay']
-    error_samples = np.reshape(error_dist.rvs(size=n_optim_runs*7), (n_optim_runs, 7))
     parameter_samples = error_samples * baseline_beta
 
     ol_objs = np.zeros(n_optim_runs)
@@ -121,10 +127,12 @@ def run_optimisations(ensemble_and_fit, params, setup, n_optim_runs, standard_de
         sim_model.params['inf_tanoak_to_bay'] = parameter_samples[i, 5]
         sim_model.params['inf_bay_to_bay'] = parameter_samples[i, 6]
 
-        sim_run = sim_model.run_policy(control_policy=ol_control)
-        ol_objs[i] = 0.0 # TODO need to get objective from simulation run
+        _, obj, _ = sim_model.run_policy(control_policy=ol_control)
+        ol_objs[i] = obj
 
         print("Open-loop run {0} done.".format(i))
+
+    mpc_args['init_policy'] = ol_control
 
     mpc_objs = np.zeros(n_optim_runs)
     mpc_controls = np.zeros((n_optim_runs, 9, len(setup['times'])))
@@ -135,8 +143,8 @@ def run_optimisations(ensemble_and_fit, params, setup, n_optim_runs, standard_de
         sim_model.params['inf_bay_to_bay'] = parameter_samples[i, 6]
 
         mpc_controller = mpc.Controller(sim_model, approx_model)
-        _, _, mpc_control = mpc_controller.run_controller(**mpc_args)
-        mpc_objs[i] = 0.0 # TODO need to get objective from simulation run
+        _, _, mpc_control, mpc_obj = mpc_controller.run_controller(**mpc_args)
+        mpc_objs[i] = mpc_obj
         mpc_controls[i] = mpc_control
 
         print("MPC run {0} done.".format(i))
@@ -156,13 +164,13 @@ def run_all():
     params = parameters.CORRECTED_PARAMS
 
     # Standard deviation for parameters as proportion of parameter value
-    standard_dev = 0.1
+    standard_dev = 0.5
 
     # Number of simulation ensemble runs for fitting
     n_ensemble_runs = 10
 
     # Number of parameter samples for control optimisation
-    n_optimisation_runs = 100
+    n_optimisation_runs = 10
 
     # Initial conditions used in 2012 paper (not quite at dynamic equilibrium) for Fig 4a
     S11, S12, S13, S14, S2, S3 = parameters.COBB_INIT_FIG4A
@@ -190,9 +198,22 @@ def run_all():
     params['payoff_factor'] = 1.0
     params['discount_rate'] = 0.0
 
-    ensemble_and_fit = generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev)
-    np.savez_compressed("fitting_ensemble_data", **ensemble_and_fit)
+    mpc_args = {
+        'horizon': 100,
+        'time_step': 0.5,
+        'end_time': 100,
+        'update_period': 5,
+        'rolling_horz': False,
+        'stage_len': 5,
+        'init_policy': even_policy
+    }
 
+    ensemble_and_fit = generate_ensemble_and_fit(params, setup, n_ensemble_runs, standard_dev)
+    np.savez_compressed("fitting_ensemble_data_0.5", **ensemble_and_fit)
+
+    optimisations = run_optimisations(
+        ensemble_and_fit, params, setup, n_optimisation_runs, standard_dev, **mpc_args)
+    np.savez_compressed("optimisation_data_0.5", **optimisations)
 
 if __name__ == "__main__":
     run_all()
