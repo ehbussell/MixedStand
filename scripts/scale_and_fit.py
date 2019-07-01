d95544b7749f4813bfd4190852ada446561a2d5b
Elliott Bussell <ehb31@cam.ac.uk>
2019-07-01 11:22:45 +0100

    Control scaling scan

--- scripts/scale_and_fit.py
+++ scripts/scale_and_fit.py
@@ -69,7 +69,10 @@ def scale_control():
 
     # First run simulations for range of roguing rates, with constant control rates
     setup, params = utils.get_setup_params(
-        parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A)
+        parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A,
+        extra_spread=False)
+
+    params['max_budget'] = 1000
 
     with open(os.path.join("data", "scale_and_fit_results.json"), "r") as infile:
         scale_and_fit_results = json.load(infile)
@@ -93,7 +96,9 @@ def scale_control():
         approx_tans = np.zeros_like(test_rates)
 
         setup, params = utils.get_setup_params(
-            parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A)
+            parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A,
+            extra_spread=False)
+        params['max_budget'] = 1000
 
         beta_names = ['beta_1,1', 'beta_1,2', 'beta_1,3', 'beta_1,4',
                       'beta_12', 'beta_21', 'beta_2']
@@ -121,9 +126,11 @@ def main(filename):
 
     # Set parameters and initial conditions
     _, old_params = utils.get_setup_params(
-        parameters.COBB_PARAMS, scale_inf=False, host_props=parameters.COBB_PROP_FIG4A)
+        parameters.COBB_PARAMS, scale_inf=False, host_props=parameters.COBB_PROP_FIG4A,
+        extra_spread=False)
     setup, new_params = utils.get_setup_params(
-        parameters.CORRECTED_PARAMS, scale_inf=False, host_props=parameters.COBB_PROP_FIG4A)
+        parameters.CORRECTED_PARAMS, scale_inf=False, host_props=parameters.COBB_PROP_FIG4A,
+        extra_spread=False)
 
     # First find beta scaling that gives matching time scale using corrected model
     scaling_factor = fitting.scale_sim_model(setup, old_params, new_params, time_step=0.001)
@@ -136,7 +143,8 @@ def main(filename):
         json.dump(results, outfile, indent=4)
 
     setup, params = utils.get_setup_params(
-        parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A)
+        parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A,
+        extra_spread=False)
 
     tanoak_factors, beta = fit_beta(setup, params)
 
@@ -159,7 +167,8 @@ def run_scan(filename):
     factors = np.arange(0.5, 5.05, 0.05)
 
     setup, new_params = utils.get_setup_params(
-        parameters.CORRECTED_PARAMS, scale_inf=False, host_props=parameters.COBB_PROP_FIG4A)
+        parameters.CORRECTED_PARAMS, scale_inf=False, host_props=parameters.COBB_PROP_FIG4A,
+        extra_spread=False)
     setup['times'] = np.arange(0, 50, step=0.01)
 
     cross_over_times = []
@@ -185,6 +194,73 @@ def run_scan(filename):
         spamwriter.writerow(['ScalingFactor', 'CrossOverTime'])
         for i, factor in enumerate(factors):
             spamwriter.writerow([factor, cross_over_times[i]])
+
+def run_scan_control(filename):
+    """Scan over control scaling factor."""
+
+    factors = np.arange(0.5, 1.51, 0.01)
+    test_rates = np.linspace(0, 1.0, 51)
+
+    diff_results = np.zeros_like(factors)
+
+    # Run simulations for range of roguing rates, with constant control rates
+    setup, params = utils.get_setup_params(
+        parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A,
+        extra_spread=False)
+
+    params['max_budget'] = 1000
+
+    with open(os.path.join("data", "scale_and_fit_results.json"), "r") as infile:
+        scale_and_fit_results = json.load(infile)
+
+    sim_tans = np.zeros_like(test_rates)
+    ncells = np.product(setup['landscape_dims'])
+
+    for i, rate in enumerate(test_rates):
+        params['rogue_rate'] = rate
+        model = ms_sim.MixedStandSimulator(setup, params)
+
+        sim_run = model.run_policy(const_rogue_policy)
+        sim_state = np.sum(sim_run[0].reshape((ncells, 15, -1)), axis=0) / ncells
+        sim_tans[i] = np.sum(sim_state[[6, 8, 9, 11], -1])
+
+        logging.info("Sim run, rate: %f, healthy tans: %f", rate, sim_tans[i])
+
+    def min_func(factor):
+        """Function to minimise, SSE between healthy tanoak over range of rates."""
+
+        approx_tans = np.zeros_like(test_rates)
+
+        setup, params = utils.get_setup_params(
+            parameters.CORRECTED_PARAMS, scale_inf=True, host_props=parameters.COBB_PROP_FIG4A,
+            extra_spread=False)
+        params['max_budget'] = 1000
+
+        beta_names = ['beta_1,1', 'beta_1,2', 'beta_1,3', 'beta_1,4',
+                      'beta_12', 'beta_21', 'beta_2']
+        beta = np.array([scale_and_fit_results[x] for x in beta_names])
+
+        for i, rate in enumerate(test_rates):
+            params['rogue_rate'] = rate * factor
+            approx_model = ms_approx.MixedStandApprox(setup, params, beta)
+            approx_run = approx_model.run_policy(const_rogue_policy)
+
+            approx_tans[i] = np.sum(approx_run[0][[6, 8, 9, 11], -1])
+
+            logging.info("Approx run, Factor %f, Rate: %f, tans: %f", factor, rate, approx_tans[i])
+
+        return np.sum(np.square(approx_tans - sim_tans))
+
+    for i, factor in enumerate(factors):
+        diff = min_func(factor)
+        diff_results[i] = diff
+
+    csv_file = filename + '_scan_control.csv'
+    with open(csv_file, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['ControlScalingFactor', 'SSE'])
+        for i, factor in enumerate(factors):
+            spamwriter.writerow([factor, diff_results[i]])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -216,6 +292,7 @@ if __name__ == "__main__":
 
     if args.scan:
         run_scan(filepath)
+        run_scan_control(filepath)
     else:
         main(filepath)
 
